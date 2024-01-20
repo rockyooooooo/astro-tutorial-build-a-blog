aa10e6e5818c25f847c3b74bdde1dc72f6def7c6
Allen Liao <namo50095@gmail.com>
2024-01-20 17:03:54 +0800

    Add an RSS feed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://timely-lamington-1fa743.netlify.app'
+});
